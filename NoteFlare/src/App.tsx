5b38f9304cbd9f73e4eea21ea2adb6d4c4a01631
sahasourav17 <sahasourav1170@gmail.com>
2023-02-26 13:24:03 +0600

    add edit note functionality

--- NoteFlare/src/App.tsx
+++ NoteFlare/src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from "uuid";
 import { ViewNotes } from "./components/ViewNotes";
 import { NoteLayout } from "./components/NoteLayout";
 import { Note } from "./components/Note";
+import { EditNote } from "./components/EditNote";
 
 export type Note = {
   id: string;
@@ -56,6 +57,18 @@ function App() {
     });
   }
 
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) };
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -79,7 +92,16 @@ function App() {
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
           <Route index element={<Note />} />
-          <Route path="edit" element={<h1>Edit</h1>} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
--- NoteFlare/src/components/Note.tsx
+++ NoteFlare/src/components/Note.tsx
@@ -1,4 +1,6 @@
-import { Badge, Col, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Link } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 
 export function Note() {
@@ -18,7 +20,19 @@ export function Note() {
             </Stack>
           )}
         </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </Link>
+            <Button variant="outline-danger">Delete</Button>
+            <Link to="/">
+              <Button variant="outline-secondary">Back</Button>
+            </Link>
+          </Stack>
+        </Col>
       </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
   );
 }

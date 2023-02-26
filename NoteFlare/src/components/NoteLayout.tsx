69943db2008d32082b1e89acf5f1ff54f07a7b1e
sahasourav17 <sahasourav1170@gmail.com>
2023-02-26 12:52:26 +0600

    fix not showing notes when clicked

--- NoteFlare/src/components/NoteLayout.tsx
+++ NoteFlare/src/components/NoteLayout.tsx
@@ -1,4 +1,9 @@
-import { Navigate, Outlet, useParams } from "react-router-dom";
+import {
+  Navigate,
+  Outlet,
+  useOutletContext,
+  useParams,
+} from "react-router-dom";
 import { Note } from "../App";
 
 type NoteLayoutProps = {
@@ -7,10 +12,14 @@ type NoteLayoutProps = {
 
 export function NoteLayout({ notes }: NoteLayoutProps) {
   const { id } = useParams();
-  const note = notes.find((note) => note.id === id);
+  const note = notes.find((n) => n.id === id);
 
   if (note == null) {
     return <Navigate to="/" replace />;
   }
   return <Outlet context={note} />;
+}
+
+export function useNote() {
+  return useOutletContext<Note>();
 }
